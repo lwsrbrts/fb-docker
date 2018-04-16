b03bb4821b1c2e40f68f76e221a1f90516089be4
lwsrbrts <lewis@lewisroberts.com>
2018-04-16 18:13:11 +0100

    add headers

--- init_container.sh
+++ init_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get environment variables to show up in SSH session
-eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
+#eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 
 service ssh start
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
